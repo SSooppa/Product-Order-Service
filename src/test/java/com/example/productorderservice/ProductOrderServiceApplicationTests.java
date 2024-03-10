f8856033cbd7493c0b353d72dbb12f5c93b93261
YeonHo <yhjeong0002@gamil.com>
2024-03-10 23:39:41 +0900

    프로젝트 시작

---
+++ src/test/java/com/example/productorderservice/ProductOrderServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.productorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }}
